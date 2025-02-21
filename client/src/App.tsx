84075d5d7ead4b3f3f11e6abe5b8a4056b6e2157
ps506101 <39854366-ps506101@users.noreply.replit.com>
2025-02-21 01:39:24 +0000

    Agent query: Is the new banner visible at the top of the page with the provided image?
    Add a banner component to the homepage.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/8a5923b0-78e1-4543-89c2-008636005321/444f2009-dae2-4fce-8a1f-3bb2e42c36f7.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,17 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
+import Banner from "@/components/banner";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <Banner />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
